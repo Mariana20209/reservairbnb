02ba679f117239830c14b20f9c5f9909d65b652f
Mariana20209 <mariasuarez20209@gmail.com>
2026-04-15 19:27:35 -0500

    Estructura del proyecto

---
+++ reservairbnb/src/main/java/com/reservairbnb/reservairbnb/ReservairbnbApplication.java
@@ -0,0 +1,13 @@
+package com.reservairbnb.reservairbnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservairbnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservairbnbApplication.class, args);
+	}
+
+}
---
+++ reservairbnb/src/test/java/com/reservairbnb/reservairbnb/ReservairbnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservairbnb.reservairbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservairbnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
